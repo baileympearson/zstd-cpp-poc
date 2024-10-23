cb56e54b02dcb25ad1bb1bafa7be5d64cc691449
Bailey Pearson <bailey.pearson@mongodb.com>
2024-10-23 14:14:02 -0600

    cleanup & comment

--- src/addon.cpp
+++ src/addon.cpp
@@ -7,7 +7,7 @@
 
 using namespace Napi;
 
-typedef std::vector<uint8_t> byte_vec;
+typedef std::vector<uint8_t> vector_of_bytes;
 
 struct FrameHeader
 {
@@ -28,42 +28,36 @@ struct FrameHeader
 	unsigned _reserved1;
 	unsigned _reserved2;
 
-	FrameHeader(Uint8Array array);
+	FrameHeader(Uint8Array array) : FrameHeader(parseFrameHeader(array)) {};
 
 private:
-	FrameHeader(std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> ptr);
+	FrameHeader(std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> ptr) : frameContentSize(ptr->frameContentSize),
+																							windowSize(ptr->windowSize),
+																							frameType(ptr->frameType),
+																							headerSize(ptr->headerSize),
+																							dictID(ptr->dictID),
+																							checksumFlag(ptr->checksumFlag)
 
-	static std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> parseFrameHeader(Uint8Array buffer);
+	{};
+
+	static std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> parseFrameHeader(Uint8Array buffer)
+	{
+		std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> ptr((ZSTD_frameHeader *)malloc(sizeof(ZSTD_frameHeader)));
+		auto result = ZSTD_getFrameHeader(ptr.get(), buffer.Data(), buffer.ByteLength());
+
+		if (ZSTD_isError(result))
+		{
+			std::string error_message = std::string("Error parsing frame header: ") + ZSTD_getErrorName(result);
+			throw Error::New(buffer.Env(), error_message);
+		}
+
+		return ptr;
+	}
 };
 
-FrameHeader::FrameHeader(std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> ptr) : frameContentSize(ptr->frameContentSize),
-																						windowSize(ptr->windowSize),
-																						frameType(ptr->frameType),
-																						headerSize(ptr->headerSize),
-																						dictID(ptr->dictID),
-																						checksumFlag(ptr->checksumFlag)
-
-{};
-
-std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> FrameHeader::parseFrameHeader(Uint8Array buffer)
+vector_of_bytes decompress(vector_of_bytes &compressed, size_t buffer_size)
 {
-	std::unique_ptr<ZSTD_frameHeader, FrameHeader::Deleter> ptr((ZSTD_frameHeader *)malloc(sizeof(ZSTD_frameHeader)));
-	auto result = ZSTD_getFrameHeader(ptr.get(), buffer.Data(), buffer.ByteLength());
-
-	if (ZSTD_isError(result))
-	{
-		std::string error_message = std::string("Error parsing frame header: ") + ZSTD_getErrorName(result);
-		throw Error::New(buffer.Env(), error_message);
-	}
-
-	return ptr;
-}
-
-FrameHeader::FrameHeader(Uint8Array array) : FrameHeader(parseFrameHeader(array)) {};
-
-byte_vec decompress(byte_vec &compressed, size_t buffer_size)
-{
-	byte_vec decompressed(buffer_size);
+	vector_of_bytes decompressed(buffer_size);
 
 	size_t _result = ZSTD_decompress(decompressed.data(), decompressed.size(), compressed.data(), compressed.size());
 
@@ -78,15 +72,10 @@ byte_vec decompress(byte_vec &compressed, size_t buffer_size)
 	return decompressed;
 }
 
-byte_vec decompress(byte_vec &compressed)
-{
-	return decompress(compressed, compressed.size() * 50);
-}
-
-byte_vec compress(byte_vec &to_compress, size_t compression_level)
+vector_of_bytes compress(vector_of_bytes &to_compress, size_t compression_level)
 {
 	auto output_buffer_size = ZSTD_compressBound(to_compress.size());
-	byte_vec output(output_buffer_size);
+	vector_of_bytes output(output_buffer_size);
 
 	size_t _result = ZSTD_compress(output.data(), output.size(), to_compress.data(), to_compress.size(), compression_level);
 
@@ -99,12 +88,6 @@ byte_vec compress(byte_vec &to_compress, size_t compression_level)
 	output.resize(_result);
 
 	return output;
-}
-
-byte_vec compress(std::string &to_compress, size_t compression_level)
-{
-	byte_vec data(to_compress.begin(), to_compress.end());
-	return compress(data, compression_level);
 }
 
 Uint8Array Uint8ArrayFromValue(Napi::Value v, std::string argument_name)
@@ -130,30 +113,21 @@ void copy_buffer_data(T *source, T *dest, size_t count)
 class CompressWorker : public Napi::AsyncWorker
 {
 public:
-	CompressWorker(const Napi::Env& env, const byte_vec data, size_t compression_level)
+	CompressWorker(const Napi::Env &env, const vector_of_bytes data, size_t compression_level)
 		: Napi::AsyncWorker{env, "CompressWorker"},
 		  m_deferred{env},
 		  data(data),
 		  compression_level(compression_level),
 		  result{} {}
 
-	/**
-	 * GetPromise associated with _deferred for return to JS
-	 */
 	Napi::Promise GetPromise() { return m_deferred.Promise(); }
 
 protected:
-	/**
-	 * Simulate heavy math work
-	 */
 	void Execute()
 	{
 		result = compress(data, compression_level);
 	}
 
-	/**
-	 * Resolve the promise with the result
-	 */
 	void OnOK()
 	{
 		Uint8Array output = Uint8Array::New(Env(), result.size());
@@ -163,9 +137,6 @@ protected:
 		m_deferred.Resolve(output);
 	}
 
-	/**
-	 * Reject the promise with errors
-	 */
 	void OnError(const Napi::Error &err)
 	{
 		m_deferred.Reject(err.Value());
@@ -173,38 +144,28 @@ protected:
 
 private:
 	Napi::Promise::Deferred m_deferred;
-	byte_vec data;
+	vector_of_bytes data;
 	size_t compression_level;
-	byte_vec result;
+	vector_of_bytes result;
 };
 
 class DecompressWorker : public Napi::AsyncWorker
 {
 public:
-	DecompressWorker(const Napi::Env& env, const byte_vec data, size_t buffer_size)
+	DecompressWorker(const Napi::Env &env, const vector_of_bytes data, size_t buffer_size)
 		: Napi::AsyncWorker{env, "DecompressWorker"},
 		  m_deferred{env},
 		  data(data),
 		  buffer_size(buffer_size),
 		  result(buffer_size) {}
 
-	/**
-	 * GetPromise associated with _deferred for return to JS
-	 */
 	Napi::Promise GetPromise() { return m_deferred.Promise(); }
-
 protected:
-	/**
-	 * Simulate heavy math work
-	 */
 	void Execute()
 	{
 		result = decompress(data, buffer_size);
 	}
 
-	/**
-	 * Resolve the promise with the result
-	 */
 	void OnOK()
 	{
 		Uint8Array output = Uint8Array::New(Env(), result.size());
@@ -214,9 +175,6 @@ protected:
 		m_deferred.Resolve(output);
 	}
 
-	/**
-	 * Reject the promise with errors
-	 */
 	void OnError(const Napi::Error &err)
 	{
 		m_deferred.Reject(err.Value());
@@ -224,9 +182,9 @@ protected:
 
 private:
 	Napi::Promise::Deferred m_deferred;
-	byte_vec data;
+	vector_of_bytes data;
 	size_t buffer_size;
-	byte_vec result;
+	vector_of_bytes result;
 };
 
 Napi::Promise Compress(const Napi::CallbackInfo &info)
@@ -267,13 +225,12 @@ Napi::Promise Compress(const Napi::CallbackInfo &info)
 
 	copy_buffer_data(input_data, data.data(), total);
 
-	CompressWorker* worker = new CompressWorker(
+	CompressWorker *worker = new CompressWorker(
 		info.Env(),
 		data,
-		compression_level
-	);
+		compression_level);
 
-	worker -> Queue();
+	worker->Queue();
 
 	return worker->GetPromise();
 }
@@ -301,21 +258,20 @@ Napi::Promise Decompress(const CallbackInfo &info)
 	uint8_t *input_data = compressed_data.Data();
 	size_t total = compressed_data.ElementLength();
 
-	byte_vec data(total);
+	vector_of_bytes data(total);
 	copy_buffer_data(input_data, data.data(), total);
 
-
-	DecompressWorker* worker = new DecompressWorker(
+	DecompressWorker *worker = new DecompressWorker(
 		info.Env(),
 		data,
-		total * 100
-	);
+		total * 100);
 
-	worker -> Queue();
+	worker->Queue();
 
 	return worker->GetPromise();
 }
 
+/** useful for debugging */
 Value getFrameHeader(const CallbackInfo &info)
 {
 	if (info.Length() != 1)
